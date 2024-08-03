f1c4b905243c03564932d7de28200a68af5f926d
Karema <karemahamdy51@gmail.com>
2024-08-04 02:16:39 +0300

    fix: isuue in home style

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,8 +3,8 @@ import { NavLink } from "react-router-dom";
 function Nav() {
   return (
     <>
-      <nav className="mt-">
-        <div className="bg-red-500 rounded-full max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <nav className="mt-20">
+        <div className="bg-white rounded-full max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             <button type="button" className="rounded-full text-white bg-[var(--primary)] font-medium text-sm px-3 py-3 text-center">Contact Us</button>
             <button data-collapse-toggle="navbar-cta" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import Nav from '../../components/Nav.jsx';
 function Home () {
   return (
-    <div className='home'>
+    <div className=''>
       <div className='header'></div>
         <Nav/>
     <h1> hello </h1>
