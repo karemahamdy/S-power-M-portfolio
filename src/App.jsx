a18b2a755030d5dff2daa3a79eed72dfc860a84e
Karema <karemahamdy51@gmail.com>
2024-08-03 23:56:20 +0300

    build: confi files

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import targetIcon from "./assets/images/target (1).svg";
 
 function App() {
 
@@ -8,6 +9,8 @@ function App() {
       <p className="read-the-docs text-center bg-pink-600">
         Click on the Vite and React logos to learn more
       </p>
+      <button className='bg-[#2BB57333]'>test</button>
+      <img src={targetIcon}/>
     </>
   )
 }
