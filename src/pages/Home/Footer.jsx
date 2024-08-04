1c0ce66d251467ba0336e17d9a060a88fdbebe1e
Karema <karemahamdy51@gmail.com>
2024-08-04 14:53:59 +0300

    add footer

---
+++ src/pages/Home/Footer.jsx
@@ -0,0 +1,43 @@
+import { NavLink } from "react-router-dom";
+import IntroImage from "../../assets/images/Layer 2.png";
+function Footer () {
+  return (
+
+<footer className="bg-secondary">
+    <div className="flex flex-row justify-between p-20">
+    <div className="flex flex-col space-y-3 font-medium">
+        <img src={IntroImage} alt="intro" />
+        <p className="text-white">All rights Reserved | 2023</p>
+    </div>
+    
+
+    <ul className=" font-light space-y-5">
+          
+              <li>
+                <NavLink to="/about" className={({ isActive }) => `block text-white ${isActive ? 'active-link' : ''}`}>
+                  About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/services" className={({ isActive }) => `block text-white ${isActive ? 'active-link' : ''}`}>
+                  Services
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact" className={({ isActive }) => `block text-white ${isActive ? 'active-link' : ''}`}>
+                  Contact
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/portfolio" className={({ isActive }) => `block text-white ${isActive ? 'active-link' : ''}`}>
+                  Portfolio
+                </NavLink>
+              </li>
+            </ul>
+    </div>
+
+  
+</footer>
+  )}
+
+  export default Footer
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Hero from "../../pages/Home/Hero.jsx"
 import Intro from "../../pages/Home/Intro.jsx"
 import Contact from "../../pages/Home/Contact.jsx"
 import Clients from "../../pages/Home/Clients.jsx"
+import Footer from "../../pages/Home/Footer.jsx"
 
 function Home () {
   return (
@@ -17,6 +18,7 @@ function Home () {
         <hr></hr>
     </div>
         <Contact/>
+        <Footer/>
   </>
   )
 }
