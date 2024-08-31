af50b1bedd7baf7b6cecd185594959bbdf9859cb
Karema <karemahamdy51@gmail.com>
2024-09-01 02:25:22 +0300

    fix: header style to add heading

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,10 @@ const Header = () => {
                   <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                 </svg>
               </div>
-              <span className="text-sm text-gray-600">+2010 26 75 48 31</span>
+              <div className="flex flex-col">
+              <p className="text-gray-400">phone number</p>
+              <span className="text-sm text-gray-600 font-bold">+2010 26 75 48 31</span>
+            </div>
             </div>
             <div className="flex items-center">
               <div className="bg-gray-100 rounded-full p-2 mr-2">
@@ -25,7 +28,10 @@ const Header = () => {
                   <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
                 </svg>
               </div>
-              <span className="text-sm text-gray-600">info@spowerm-corp.com</span>
+              <div className="flex flex-col">
+              <p className="text-gray-400">Email</p>
+              <span className="text-sm text-gray-600 font-bold">info@spowerm-corp.com</span>
+            </div>
             </div>
             <div className="flex items-center">
               <div className="bg-gray-100 rounded-full p-2 mr-2">
@@ -33,7 +39,10 @@ const Header = () => {
                   <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
                 </svg>
               </div>
-              <span className="text-sm text-gray-600">To be Added</span>
+              <div className="flex flex-col">
+              <p className="text-gray-400">Location</p>
+              <span className="text-sm text-gray-600 font-bold">To be Added</span>
+              </div>
             </div>
           </div>
         </div>
