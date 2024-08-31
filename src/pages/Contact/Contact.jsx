0fd31b049fe5592af8e02a83be50007a3fd67226
Karema <karemahamdy51@gmail.com>
2024-09-01 02:17:21 +0300

    feat: add footer to contact

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,12 +1,13 @@
 import BackGround from "../../components/Background"
 import Contact from "../../pages/Home/Contact.jsx"
+import Footer from "../../pages/Home/Footer.jsx"
 function About () {
   return (
     <>
     <BackGround>Contact Us</BackGround>
     
     <Contact/>
-  
+    <Footer/>
   </>
   )
 }
